c52cfd5b1e55bc8747784a46a24247e073e1d817
DmitryKaga2K <dmitryk27@gmail.com>
2023-10-09 16:41:23 -0400

    Main.js data/teams

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 const dataLoader = require("./dataLoader");
 const generator = require("./generator");
 const fs = require("fs");
+
+const teamsData = dataLoader.loadTeams("data/teams.json");
+const playersData = dataLoader.loadPlayers("data/players.json");
+const scheduleData = dataLoader.loadSchedule("data/schedule.json");
